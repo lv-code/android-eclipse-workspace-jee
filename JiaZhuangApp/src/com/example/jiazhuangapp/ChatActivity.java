b6f31215b6f097c2798bc4093b19716cf5056fc1
lv-code <lvcode8@gmail.com>
2014-11-05 17:31:01 +0800

    目标要动态控制ListView的layout_above

--- JiaZhuangApp/src/com/example/jiazhuangapp/ChatActivity.java
+++ JiaZhuangApp/src/com/example/jiazhuangapp/ChatActivity.java
@@ -1,7 +1,10 @@
 package com.example.jiazhuangapp;
 
+import static android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
@@ -9,6 +12,7 @@ import java.util.List;
 import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -22,6 +26,7 @@ import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
@@ -33,7 +38,6 @@ import android.widget.Toast;
 
 public class ChatActivity extends Activity implements OnClickListener {
 
-	private static final int SHOW_ALBUM = 11;
 
 	private boolean isBig = false;
 
@@ -41,6 +45,7 @@ public class ChatActivity extends Activity implements OnClickListener {
 	private Button mBtnBack;
 	private ImageView mImgAddition;
 	private ImageView mImgAlbum;
+	private ImageView mImgCamera;
 	private RelativeLayout mLayout;
 	private EditText mEditTextContent;
 	private ListView mListView;
@@ -50,9 +55,14 @@ public class ChatActivity extends Activity implements OnClickListener {
 	// 从相册选择照片后，裁剪保存的路径
 	private static final String IMAGE_FILE_LOCATION = "file:///sdcard/temp.jpg"; // temp
 																					// file
+	private static final int SHOW_ALBUM = 11;
 	private Uri imageUri = Uri.parse(IMAGE_FILE_LOCATION);// The Uri to store
 	private Bitmap imgBitmap; //获取发送的图片转化为Bitmap
 
+	private Uri photoUri;
+	private final static int TAKE_PHOTO = 10;
+	private final static String PHOTO_URI = "photoUri";
+	
 	private final String MY_NAME = "麦兜";
 
 	public void onCreate(Bundle savedInstanceState) {
@@ -79,6 +89,7 @@ public class ChatActivity extends Activity implements OnClickListener {
 		mImgAddition = (ImageView) findViewById(R.id.btn_addition_menu);
 		mLayout = (RelativeLayout) findViewById(R.id.rl_bottom);
 		mImgAlbum = (ImageView) findViewById(R.id.chat_phone_album);
+		mImgCamera = (ImageView) findViewById(R.id.chat_camera);
 	}
 
 	private void initListener() {
@@ -87,6 +98,10 @@ public class ChatActivity extends Activity implements OnClickListener {
 			@Override
 			public void onClick(View v) {
 				togglePanelAddition();
+				// TODO 重新加载Adapter，让ListView在RelativeLayout上面
+				ViewGroup.LayoutParams vgLayoutParams = (ViewGroup.LayoutParams) mListView.getLayoutParams();
+				RelativeLayout.LayoutParams rlLayoutParams =  new RelativeLayout.LayoutParams();
+				vgLayoutParams.layout_above = 
 			}
 		});
 
@@ -115,6 +130,18 @@ public class ChatActivity extends Activity implements OnClickListener {
 				intent.putExtra("noFaceDetection", true); // no face detection
 				startActivityForResult(intent, SHOW_ALBUM);
 
+			}
+		});
+		
+		mImgCamera.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+				photoUri = getContentResolver().insert(EXTERNAL_CONTENT_URI,
+						new ContentValues());
+				intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+				startActivityForResult(intent, TAKE_PHOTO);
 			}
 		});
 		chkSDStatus();
@@ -169,38 +196,21 @@ public class ChatActivity extends Activity implements OnClickListener {
 				}
 				send();
 				togglePanelAddition();
-				/*
-				 * 直接从相册获取图片 ContentResolver resolver = getContentResolver();
-				 * try { // 照片的原始资源地址 imgUri = data.getData(); } catch
-				 * (Exception e) {
-				 * 
-				 * System.out.println(e.getMessage()); } Bitmap photo = null;
-				 * System.out.println("data111111-->" + data);
-				 * System.out.println("imgUri -->" + imgUri); try { //
-				 * 使用ContentProvider通过Uri获取原始图片 photo =
-				 * MediaStore.Images.Media.getBitmap(resolver, imgUri);
-				 * 
-				 * } catch (IOException e) { // TODO: handle exception
-				 * e.printStackTrace(); System.out.println("data44444-->" +
-				 * data); }
-				 * 
-				 * // 获取屏幕分辨率 DisplayMetrics dm_2 = new DisplayMetrics();
-				 * getWindowManager().getDefaultDisplay().getMetrics(dm_2);
-				 * 
-				 * // 图片分辨率与屏幕分辨率 float scale_2 = photo.getWidth() / (float)
-				 * dm_2.widthPixels;
-				 * 
-				 * Bitmap newBitMap_2 = null; if (scale_2 > 1) { newBitMap_2 =
-				 * zoomBitmap(photo, photo.getWidth() / scale_2,
-				 * photo.getHeight() / scale_2); photo.recycle(); isBig = true;
-				 * }
-				 */
-				System.out.println("data22222-->" + data);
-				// TODO 将照片显示到ListView中
-				// 发图片用新的xml来显示，左右各一个
 
 				break;
-
+			case TAKE_PHOTO:
+				try {
+					InputStream stream = getContentResolver().openInputStream(
+							photoUri);
+					if (photoUri != null){
+						imgBitmap = BitmapFactory.decodeStream(stream);
+					}
+					send();
+					togglePanelAddition();
+				} catch (FileNotFoundException e) {
+					Log.e("TitlePageActivity", "FileNotFound", e);
+				}
+				break;
 			default:
 				System.out.println("data33333-->" + data);
 				break;
@@ -223,10 +233,6 @@ public class ChatActivity extends Activity implements OnClickListener {
 	}
 
 	/*
-	 * "即使是一块牛肉，也应该有自己的态度，要慎其独，要善其身，要知道精彩的人生不能只有精肉，还要有适宜的肥油做调配，有雪白的肉筋做环绕，并且还要掌握跳进煎锅时的角度，姿势，以及火候，才能最终成为一块优秀道地的西冷牛排~"
-	 * , "不开心睡一觉，就让他过去吧，伤心还好，伤胃就不好了。", "要懂得珍惜守护身边的每个人，因为前世扭断脖子的回眸，我们才换来了今生的相遇；",
-	 * "我们可以慢慢向前走，但绝对不能后退。永远不要后退，退到最后是无路可退；", "感谢身材，即使臃肿，我也能到世界各地去旅游；",
-	 * "感谢那些曾让我伤心难过的日子，我知道快乐已经离我不远了；", "蓬莱（pénglái）文章建安骨"
 	 */
 	private String[] msgArray = new String[] { "我的志愿是", "做一名校长",
 			"每天收集了学生的学费之后就去吃火锅", "今天吃麻辣火锅", "明天吃酸菜鱼火锅", "后天吃猪骨头火锅", "老师直夸我：",
@@ -236,7 +242,7 @@ public class ChatActivity extends Activity implements OnClickListener {
 			"2012-09-01 18:10", "2012-09-01 18:11", "2012-09-01 18:20",
 			"2012-09-01 18:30", "2012-09-01 18:35", "2012-09-01 18:40",
 			"2012-09-01 18:50" };
-	private final static int COUNT = 0;
+	private final static int COUNT = 8;
 
 	public void initData() {
 		for (int i = 0; i < COUNT; i++) {
