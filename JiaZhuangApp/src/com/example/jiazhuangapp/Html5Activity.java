c17181f3d12cd61f6bf53cd07e1466873cecf305
lv-code <lvcode8@gmail.com>
2015-01-06 16:00:36 +0800

    add some files

---
+++ JiaZhuangApp/src/com/example/jiazhuangapp/Html5Activity.java
@@ -0,0 +1,15 @@
+package com.example.jiazhuangapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.beta.main.R;
+
+public class Html5Activity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_html5);
+	}
+}
